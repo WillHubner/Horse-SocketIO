9bb3ebff989a437117792cf11c965c531773afe3
Willian Hubner <willian.hubner@hotmail.co.uk>
2022-04-07 08:47:20 -0400

    Add port to connect

--- src/Horse.SocketIO.pas
+++ src/Horse.SocketIO.pas
@@ -6,20 +6,38 @@ uses
   Horse,
   Horse.SocketIO.ServerSocket;
 
-procedure StartSocket(PORT : Integer);
-procedure SocketIO(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+procedure StartSocket;
+
+function SocketIO : THorseCallback; overload;
+function SocketIO(aPort : Integer) : THorseCallback; overload;
+
+procedure Middleware(Req: THorseRequest; Res: THorseResponse; Next: TProc);
 
 implementation
 
 uses
   System.SysUtils, System.JSON, Horse.SocketIO.Functions;
 
-procedure SocketIO(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+var
+  PORT : INTEGER;
+
+function SocketIO : THorseCallback;
+begin
+  Result := SocketIO(8050);
+end;
+
+function SocketIO(aPort : Integer) : THorseCallback;
+begin
+  PORT := aPort;
+  Result := Middleware;
+end;
+
+procedure Middleware(Req: THorseRequest; Res: THorseResponse; Next: TProc);
 begin
   Next;
 end;
 
-procedure StartSocket(PORT : Integer);
+procedure StartSocket;
 begin
   _ServerSocket := TSocketServer.New;
   _ServerSocket.StartServer(PORT);
@@ -28,6 +46,6 @@ begin
 end;
 
 initialization
-  StartSocket(8050);
+  StartSocket;
 
 end.
